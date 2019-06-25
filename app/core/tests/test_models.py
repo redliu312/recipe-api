ac08840d52f6864429fc6ee390f0da0020ec1c63
YenHungLiu <simon_liu@trendmicro.com>
2019-06-25 11:51:52 +0800

    add email validation test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -22,4 +22,20 @@ class ModelTests(TestCase):
                 password = password,
         )
         self.assertEqual(user.email, email.lower())
+    
+    def test_new_user_invalid_email(self):
+        email = None
+        password = "test123"
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email, password)
+
+    def test_create_new_superuser(self):
+        """test create superuser"""
+        user = get_user_model().objects.create_superuser(
+                "test@test.com",
+                "test1234"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
 
